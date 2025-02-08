59ff012faf7c8ce6c87ebcc8f3aa53ab8ac8bcd6
jai-max <kadyanaryan744@gmail.com>
2025-02-08 23:47:11 +0530

    Setup of image Generation

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className='text-4xl font-bold underline text-center'>
+      Kaise ho!
+    </h1>
+  )
+}
+
+export default App
+
