20275fe93a116e12ddce1daa6cd800f253c04587
jai-max <kadyanaryan744@gmail.com>
2025-02-12 23:24:57 +0530

    Created Post Page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
+
+import {preview} from '../assets'
+import {getRandomPrompt} from '../utils';
+import {FormField, Loader} from '../components';
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  })
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false)
   return (
+    <section className='max-w-8xl mx-auto'>
+
     <div>
       
     </div>
+    </section>
   )
 }
 
